3f4a1e8b0dbd94a8a9ea2af5a451ae69db1be17e
Mogan Xumerle <moganrz@gmail.com>
2021-06-08 08:52:19 -0300

    feat: create and insert collections

--- main.go
+++ main.go
@@ -16,6 +16,48 @@ const URI_MONGODB_ATLAS = "mongodb+srv://admin:admin@cluster0.q79nw.mongodb.net/
 
 func main() {
 
+	client, ctx := connectMongoDB()
+
+	//Create and connection on new Database and Collections
+	quickstartDatabase := client.Database("quickstart")
+	podcastsCollection := quickstartDatabase.Collection("podcasts")
+	episodesCollection := quickstartDatabase.Collection("episodes")
+
+	//Insert into collections
+	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+		{"title", "Mogancast"},
+		{"author", "Mogan Xumerle"},
+		{"tags", bson.A{"development", "programming", "coding", "finance"}},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Id PodCast Inserted: %v", podcastResult)
+
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Studying MongoDB with Golang"},
+			{"description", "Learning MongoDB"},
+			{"duration", 35},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Studying Kafka with C#"},
+			{"description", "Learning Kafka"},
+			{"duration", 43},
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+}
+
+func connectMongoDB() (*mongo.Client, context.Context) {
+
 	client, err := mongo.NewClient(options.Client().ApplyURI(URI_MONGODB_ATLAS))
 	if err != nil {
 		log.Fatal(err)
@@ -38,4 +80,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
+
+	return client, ctx
 }
