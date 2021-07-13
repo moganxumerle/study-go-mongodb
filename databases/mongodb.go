e9b5f1654068eb7768171d7e2eddf0cdabe4e37a
Mogan Xumerle <moganrz@gmail.com>
2021-07-13 08:17:10 -0300

    refactor: using context into mongodb struct

--- databases/mongodb.go
+++ databases/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,34 +13,42 @@ import (
 )
 
 type MongoDB struct {
+	ctx      context.Context
 	client   *mongo.Client
 	dataBase *mongo.Database
 }
 
-func NewMongoDB(uriMongoAtlas string, ctx context.Context) *MongoDB {
+func NewMongoDB(uriMongoAtlas string) *MongoDB {
+
+	ctx, _ := context.WithTimeout(context.Background(), 60*time.Second)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(uriMongoAtlas))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Connecting MongoDB...")
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("MongoDB connected!")
 
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	/* List Databases Name
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
+	*/
 
 	return &MongoDB{
+		ctx:    ctx,
 		client: client,
 	}
 }
@@ -52,34 +61,34 @@ func (m *MongoDB) GetCollection(collection string) *mongo.Collection {
 	return m.dataBase.Collection(collection)
 }
 
-func (m *MongoDB) Disconnect(ctx context.Context) {
-	defer m.client.Disconnect(ctx)
+func (m *MongoDB) Disconnect() {
+	defer m.client.Disconnect(m.ctx)
 }
 
-func (m *MongoDB) ReadAllCollection(ctx context.Context, collection *mongo.Collection) {
+func (m *MongoDB) ReadAllCollection(collection *mongo.Collection) {
 
-	cursor, err := collection.Find(ctx, bson.M{})
+	cursor, err := collection.Find(m.ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var documents []bson.M
-	if err = cursor.All(ctx, &documents); err != nil {
+	if err = cursor.All(m.ctx, &documents); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(documents)
 }
 
-func (m *MongoDB) ReadAllOneByOneCollection(ctx context.Context, collection *mongo.Collection) {
+func (m *MongoDB) ReadAllOneByOneCollection(collection *mongo.Collection) {
 
-	cursor, err := collection.Find(ctx, bson.M{})
+	cursor, err := collection.Find(m.ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cursor.Close(ctx)
+	defer cursor.Close(m.ctx)
 
-	for cursor.Next(ctx) {
+	for cursor.Next(m.ctx) {
 
 		var document bson.M
 
@@ -91,35 +100,70 @@ func (m *MongoDB) ReadAllOneByOneCollection(ctx context.Context, collection *mon
 	}
 }
 
-func (m *MongoDB) FindEpisodesByDuration(ctx context.Context, collection *mongo.Collection, duration int) {
+func (m *MongoDB) FindEpisodesByDuration(collection *mongo.Collection, duration int) {
 
-	filterCursor, err := collection.Find(ctx, bson.M{"duration": duration})
+	filterCursor, err := collection.Find(m.ctx, bson.M{"duration": duration})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var episodesFiltered []bson.M
-	if err = filterCursor.All(ctx, &episodesFiltered); err != nil {
+	if err = filterCursor.All(m.ctx, &episodesFiltered); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(episodesFiltered)
 }
 
-func (m *MongoDB) FindEpisodesLongerThanDurationSortByDurationDesc(ctx context.Context, collection *mongo.Collection, duration int) {
+func (m *MongoDB) FindEpisodesLongerThanDurationSortByDurationDesc(collection *mongo.Collection, duration int) {
 
 	opts := options.Find()
 	opts.SetSort(bson.D{{"duration", -1}})
 
-	filterSortCursor, err := collection.Find(ctx, bson.D{{"duration", bson.D{{"$gt", duration}}}}, opts)
+	filterSortCursor, err := collection.Find(m.ctx, bson.D{{"duration", bson.D{{"$gt", duration}}}}, opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var episodesSorted []bson.M
-	if err = filterSortCursor.All(ctx, &episodesSorted); err != nil {
+	if err = filterSortCursor.All(m.ctx, &episodesSorted); err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(episodesSorted)
+}
+
+func (m *MongoDB) InsertDocuments(podcastsCollection, episodesCollection *mongo.Collection) {
+
+	//Insert into collections
+	podcastResult, err := podcastsCollection.InsertOne(m.ctx, bson.D{
+		{"title", "Mogancast"},
+		{"author", "Mogan Xumerle"},
+		{"tags", bson.A{"development", "programming", "coding", "finance"}},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Id PodCast Inserted: %v\n", podcastResult)
+
+	episodeResult, err := episodesCollection.InsertMany(m.ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "New Studying MongoDB with Golang"},
+			{"description", "Learning MongoDB"},
+			{"duration", 35},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "New Studying Kafka with C#"},
+			{"description", "Learning Kafka"},
+			{"duration", 43},
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
 }
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"os"
 	"study-go-mongodb/databases"
-	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -23,10 +19,11 @@ var episodesCollection *mongo.Collection
 
 func main() {
 
-	ctx, _ := context.WithTimeout(context.Background(), 60*time.Second)
-
-	mongoDB := databases.NewMongoDB(URI_MONGODB_ATLAS, ctx)
-	defer mongoDB.Disconnect(ctx)
+	mongoDB := databases.NewMongoDB(URI_MONGODB_ATLAS)
+	defer func() {
+		fmt.Println("Closing MongoDB connection")
+		mongoDB.Disconnect()
+	}()
 
 	//Create and connection on new Database and Collections
 	mongoDB.CreateDatabase(DATA_BASE_NAME)
@@ -34,46 +31,11 @@ func main() {
 	episodesCollection = mongoDB.GetCollection(EPISODES_COLLECTION)
 
 	//insert
-	//insertDocuments(ctx, mongoDB)
+	mongoDB.InsertDocuments(podcastsCollection, episodesCollection)
 
 	//query
-	//mongoDB.ReadAllOneByOneCollection(ctx, podcastsCollection)
+	mongoDB.ReadAllOneByOneCollection(podcastsCollection)
 	//mongoDB.ReadAllCollection(ctx, episodesCollection)
 	//mongoDB.FindEpisodesByDuration(ctx, episodesCollection, 35)
-	mongoDB.FindEpisodesLongerThanDurationSortByDurationDesc(ctx, episodesCollection, 55)
-}
-
-func insertDocuments(ctx context.Context, mongoDB *databases.MongoDB) {
-
-	//Insert into collections
-	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
-		{"title", "Mogancast"},
-		{"author", "Mogan Xumerle"},
-		{"tags", bson.A{"development", "programming", "coding", "finance"}},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Id PodCast Inserted: %v\n", podcastResult)
-
-	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "Studying MongoDB with Golang"},
-			{"description", "Learning MongoDB"},
-			{"duration", 35},
-		},
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "Studying Kafka with C#"},
-			{"description", "Learning Kafka"},
-			{"duration", 43},
-		},
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+	//mongoDB.FindEpisodesLongerThanDurationSortByDurationDesc(episodesCollection, 25)
 }
